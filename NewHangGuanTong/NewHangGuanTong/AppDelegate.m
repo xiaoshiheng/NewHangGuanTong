e8ffab3e50453eefaf4a98b4309f3676c20ef763
xiaoshiheng <>
2024-04-19 11:04:35 +0800

    设置icon和启动页及app纯代码初始化

--- NewHangGuanTong/NewHangGuanTong/AppDelegate.m
+++ NewHangGuanTong/NewHangGuanTong/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,8 +17,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    [self.window makeKeyAndVisible];
     return YES;
 }
+
+
+#pragma mark ------- 启动的时候判断是否登录 -------
+
 
 
 
